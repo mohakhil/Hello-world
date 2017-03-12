dec0b16acb01fd763efc1bf5dec6ece6986fa309
Akhil <akhilaustin.619@gmail.com>
2017-03-11 20:23:35 -0500

    Added new condition for checking negative numbers.

--- Comparsion/src/MainClass.java
+++ Comparsion/src/MainClass.java
@@ -17,6 +17,10 @@
 	      System.out.print( "Enter second integer: " ); // prompt 
 	      number2 = input.nextInt(); // read second number from user 
 	      
+	      if ( number1 < 0 || number2 < 0 ) 
+		         System.out.printf( "one number is negative\n" );
+	      
+	      
 	      if ( number1 == number2 ) 
 	         System.out.printf( "%d == %d\n", number1, number2 );
 
