bfb6baee35303a76d983dd32a9dd782a58baa5f3
Akhil <akhilaustin.619@gmail.com>
2017-03-14 14:02:32 -0400

    deleted the lesser than or equal to logic

--- Comparsion/src/MainClass.java
+++ Comparsion/src/MainClass.java
@@ -33,8 +33,7 @@
 	      if ( number1 > number2 )
 	         System.out.printf( "%d > %d\n", number1, number2 );
 
-	      if ( number1 <= number2 )
-	         System.out.printf( "%d <= %d\n", number1, number2 );
+	   
 
 	      if ( number1 >= number2 )
 	         System.out.printf( "%d >= %d\n", number1, number2 );
