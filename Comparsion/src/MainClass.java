f5dda2390899b58481a5266e2ac8469a333875ba
Akhil Mohan <akhilaustin.619@gmail.com>
2017-03-14 14:31:06 -0400

    Add files via upload
    Replaced the two equal integers logic back from where it was deleted

--- Comparsion/src/MainClass.java
+++ Comparsion/src/MainClass.java
@@ -21,7 +21,9 @@
 		         System.out.printf( "one number is negative\n" );
 	      
 	      
-	    
+	      if ( number1 == number2 ) 
+	         System.out.printf( "%d == %d\n", number1, number2 );
+
 	      if ( number1 != number2 )
 	         System.out.printf( "%d != %d\n", number1, number2 );
 
