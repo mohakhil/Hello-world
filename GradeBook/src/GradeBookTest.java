1a6afc6b04f34181379fcf016a66264092889dc4
Akhil <akhilaustin.619@gmail.com>
2017-03-15 15:43:47 -0400

    Created GradeBook with Scanner class and a method

--- GradeBook/src/GradeBookTest.java
+++ GradeBook/src/GradeBookTest.java
@@ -1,12 +1,22 @@
 
+import java.util.Scanner;
+
 public class GradeBookTest {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		Scanner input = new Scanner( System.in );
+		
+		
 		GradeBook myGradeBook = new GradeBook();
 		
-		myGradeBook.displayMessage();
+		System.out.println( "Please enter the courseName");
+		
+		String courseName = input.nextLine();
+		System.out.println();
+		
+		myGradeBook.displayMessage( courseName );
 	}
 
 }
